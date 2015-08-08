26485b4eebf54cfae616f2dd9976b46933356a22
westonruter <westonruter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-08 06:46:54 +0000

    Update to commit e1d4d3d from https://github.com/xwp/wp-customize-partial-refresh.git
    git-svn-id: https://plugins.svn.wordpress.org/customize-partial-refresh/trunk@1215414 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/class-wp-customize-partial-refresh-widgets.php
+++ php/class-wp-customize-partial-refresh-widgets.php
@@ -251,7 +251,7 @@ class WP_Customize_Partial_Refresh_Widgets {
 			'renderWidgetQueryVar' => self::RENDER_WIDGET_QUERY_VAR,
 			'renderWidgetNonceValue' => wp_create_nonce( self::RENDER_WIDGET_AJAX_ACTION ),
 			'renderWidgetNoncePostKey' => self::RENDER_WIDGET_NONCE_POST_KEY,
-			'requestUri' => '/',
+			'requestUri' => home_url(),
 			'sidebarsEligibleForPostMessage' => $this->get_sidebars_supporting_partial_refresh(),
 			'widgetsEligibleForPostMessage' => $this->get_widgets_supporting_partial_refresh(),
 			'theme' => array(
@@ -260,8 +260,8 @@ class WP_Customize_Partial_Refresh_Widgets {
 			),
 			'previewCustomizeNonce' => wp_create_nonce( 'preview-customize_' . $wp_customize->get_stylesheet() ),
 		);
-		if ( ! empty( $_SERVER['REQUEST_URI'] ) ) {
-			$exports['requestUri'] = esc_url_raw( home_url( wp_unslash( $_SERVER['REQUEST_URI'] ) ) );
+		if ( ! empty( $_SERVER['REQUEST_URI'] ) ) { // WPCS: input var ok
+			$exports['requestUri'] = esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) ); // WPCS: input var ok
 		}
 		$wp_scripts->add_data(
 			$this->plugin->script_handles['widgets-preview'],
@@ -303,16 +303,16 @@ class WP_Customize_Partial_Refresh_Widgets {
 			if ( empty( $_POST['widget_id'] ) ) { // wpcs: input var okay
 				throw new WP_Customize_Partial_Refresh_Exception( __( 'Missing widget_id param', 'customize-partial-preview-refresh' ) );
 			}
-			$widget_id = wp_unslash( sanitize_text_field( $_POST['widget_id'] ) ); // wpcs: input var okay; sanitize_text_field for WordPress-VIP
+			$widget_id = sanitize_text_field( wp_unslash( $_POST['widget_id'] ) ); // wpcs: input var okay; sanitize_text_field for WordPress-VIP
 			if ( ! array_key_exists( $widget_id, $wp_registered_widgets ) ) {
 				throw new WP_Customize_Partial_Refresh_Exception( __( 'Unable to find registered widget', 'customize-partial-preview-refresh' ) );
 			}
 			$widget = $wp_registered_widgets[ $widget_id ];
 
-			if ( ! array_key_exists( 'sidebar_id', $_POST ) ) { // wpcs: input var okay
+			if ( empty( $_POST['sidebar_id'] ) ) { // wpcs: input var okay
 				throw new WP_Customize_Partial_Refresh_Exception( __( 'Missing sidebar_id param', 'customize-partial-preview-refresh' ) );
 			}
-			$sidebar_id = wp_unslash( sanitize_text_field( $_POST['sidebar_id'] ) ); // wpcs: input var okay; sanitize_text_field for WordPress-VIP
+			$sidebar_id = sanitize_text_field( wp_unslash( $_POST['sidebar_id'] ) ); // WPCS: input var okay; sanitize_text_field for WordPress-VIP
 			$sidebar = array();
 			if ( array_key_exists( $sidebar_id, $wp_registered_sidebars ) ) {
 				$sidebar = $wp_registered_sidebars[ $sidebar_id ];
@@ -355,13 +355,9 @@ class WP_Customize_Partial_Refresh_Widgets {
 
 			$params[0]['before_widget'] = sprintf( $params[0]['before_widget'], $widget_id, $class_name );
 			$params = apply_filters( 'dynamic_sidebar_params', $params );
-
-			$other_rendered_widget_ids = array();
-			add_action( 'dynamic_sidebar', function ( $widget ) use ( &$other_rendered_widget_ids, $widget_id ) {
-				if ( $widget_id !== $widget['id'] ) {
-					$other_rendered_widget_ids[] = $widget['id'];
-				}
-			} );
+			$this->currently_rendered_other_rendered_widget_ids = array();
+			$this->currently_rendered_widget_id = $widget_id;
+			add_action( 'dynamic_sidebar', array( $this, 'log_rendered_widget' ) );
 
 			// Render the widget
 			ob_start();
@@ -370,6 +366,8 @@ class WP_Customize_Partial_Refresh_Widgets {
 				call_user_func_array( $callback, $params );
 			}
 			$rendered_widget = ob_get_clean();
+			$other_rendered_widget_ids = $this->currently_rendered_other_rendered_widget_ids;
+
 			wp_send_json_success( compact( 'rendered_widget', 'sidebar_id', 'other_rendered_widget_ids' ) );
 		} catch ( Exception $e ) {
 			if ( $e instanceof WP_Customize_Partial_Refresh_Exception && ( defined( 'WP_DEBUG' ) && WP_DEBUG ) ) {
@@ -381,6 +379,36 @@ class WP_Customize_Partial_Refresh_Widgets {
 				$message = $generic_error;
 			}
 			wp_send_json_error( compact( 'message' ) );
+		}
+	}
+
+	/**
+	 * Workaround for lack closures.
+	 *
+	 * @see WP_Customize_Partial_Refresh_Widgets::log_rendered_widget()
+	 * @var array
+	 */
+	protected $currently_rendered_other_rendered_widget_ids = array();
+
+	/**
+	 * Workaround lack of closures.
+	 *
+	 * @see WP_Customize_Partial_Refresh_Widgets::log_rendered_widget()
+	 * @var string
+	 */
+	protected $currently_rendered_widget_id;
+
+	/**
+	 * The render_widget() method adds this as a hook for dynamic_sidebar.
+	 *
+	 * @see WP_Customize_Partial_Refresh_Widgets::render_widget()
+	 *
+	 * @param array $widget
+	 * @action dynamic_sidebar
+	 */
+	public function log_rendered_widget( $widget ) {
+		if ( $this->currently_rendered_widget_id !== $widget['id'] ) {
+			$this->currently_rendered_other_rendered_widget_ids[] = $widget['id'];
 		}
 	}
 
